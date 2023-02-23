97ffe6cc96fcf32625d5709be16ede7172de13cd
raulmahya <raulmahya11@gmail.com>
2023-02-23 09:09:10 +0700

    push chapter01

---
+++ Chapter01/site/Tugas/1214053_RaulMahya/chapter01/crot.js
@@ -0,0 +1,35 @@
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "connect.sid=s%3AMsnp_KW3uPWTf6gN4GDNl7XAoOShdRL2.VK05aaDbN1FeG%2BScGHtOuxENv5s2ABoZZzLpqN%2FUbZs");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+hasil=""
+txt=""
+txt1=""
+
+fetch("https://cat-fact.herokuapp.com/facts", requestOptions)
+  .then(response => response.text())
+  .then(result => tampilkan(result))
+  .catch(error => console.log('error', error));
+
+function tampilkan(result){
+  console.log(result);
+  hasil=JSON.parse(result);
+  txt=hasil.forEach(isitabel);
+//   document.getElementById("nama").innerHTML(result);
+}
+
+function isitabel(value){
+    const oke = "deathyear:"
+    txt= txt+trnyatabel.replace("#TEXT#","\nnama:"+value.text);
+      txt= txt+trnyatabel.replace("#TEXT#",oke+value.type+"");
+    document.getElementById("konten").innerHTML=txt;
+    }
+    trnyatabel=`
+    <div class="font-medium text-gray-700" id="name">#TEXT#</div>
+  `
+    
