26ab16d173167fbc331caf4fcca4bf591599ec79
juliardimegah <juliardimegah@gmail.com>
2023-02-23 10:46:47 +0700

    cobain

---
+++ Chapter01/site/Tugas/1214066_MegahJuliardi/coba.js
@@ -0,0 +1,36 @@
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "connect.sid=s%3AMsnp_KW3uPWTf6gN4GDNl7XAoOShdRL2.VK05aaDbN1FeG%2BScGHtOuxENv5s2ABoZZzLpqN%2FUbZs");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+hasil=""
+txt=""
+txt1=""
+
+fetch("https://api.agify.io?name=meelad", requestOptions)
+  .then(response => response.text())
+  .then(result => tampilkan(result))
+  .catch(error => console.log('error', error));
+
+function tampilkan(result){
+  console.log(result);
+  hasil=JSON.parse(result);
+  txt=hasil.forEach(isitabel);
+//   document.getElementById("nama").innerHTML(result);
+}
+
+function isitabel(value){
+    const oke = "Umur: "
+    txt= txt+trnyatabel.replace("#TEXT#","\nage: "+value.age);
+      txt= txt+trnyatabel.replace("#TEXT#",oke+value.count+"");
+      txt= txt+trnyatabel.replace("#TEXT#",oke+value.name+"");
+    document.getElementById("konten").innerHTML=txt;
+    }
+    trnyatabel=`
+    <div class="font-medium text-gray-700" id="name">#TEXT#</div>
+  `
+    
