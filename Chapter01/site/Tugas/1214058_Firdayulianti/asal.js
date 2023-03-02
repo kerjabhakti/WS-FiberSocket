bb7e41a153d737d1cd15c2fe193bf441fd3cfffc
firda <yuliantifirda966@gmail.com>
2023-03-02 08:39:15 +0700

    update

---
+++ Chapter01/site/Tugas/1214058_Firdayulianti/asal.js
@@ -0,0 +1,32 @@
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "connect.sid=s%3AMsnp_KW3uPWTf6gN4GDNl7XAoOShdRL2.VK05aaDbN1FeG%2BScGHtOuxENv5s2ABoZZzLpqN%2FUbZs");
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+hasil = "";
+txt = "";
+txt1 = "";
+
+fetch("https://api.agify.io/?name=meelad", requestOptions)
+  .then((response) => response.text())
+  .then((result) => tampilkan(result))
+  .catch((error) => console.log("error", error));
+
+function tampilkan(result) {
+  console.log(result);
+  hasil = JSON.parse(result);
+  txt = hasil.forEach(isitabel);
+}
+
+function isitabel(value) {
+  txt = txt + trnyatabel.replace("#TEXT#", "Age: " + value.age);
+  txt = txt + trnyatabel.replace("#TEXT#", "Count: " + value.count + "");
+  txt = txt + trnyatabel.replace("#TEXT#", "Name: " + value.name + "");
+  document.getElementById("konten").innerHTML = txt;
+}
+trnyatabel = `
+<div class="font-medium text-gray-700" id="name">#TEXT#</div>
+`;
